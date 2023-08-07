d2d33eb6423d39788f9992a748d6b518d43d0ccc
ShahriarAsfaq <shahriarasfaq@iut-dhaka.edu>
2023-08-07 15:30:51 +0600

    404 page add

--- src/routes/AallRoutes.jsx
+++ src/routes/AallRoutes.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import { MovieList } from "../pages/MovieList";
 import { MovieDetail } from "../pages/MovieDetail";
-import { Login } from "../pages/Login";
-import { Register } from "../pages/Register";
+import { Login } from "../pages/SignIn";
+import { Register } from "../pages/SignUP";
 import { WatchList } from "../pages/WatchList";
+import { PageNotFound } from "../pages/PageNotFound";
 
 
 export const AallRoutes = () => {
@@ -16,7 +17,7 @@ export const AallRoutes = () => {
             <Route path="pentaMovieMania/watchlist" element={<WatchList />} /> 
             <Route path="pentaMovieMania/login" element={<Login />} />
             <Route path="pentaMovieMania/register" element={<Register />} />
-            {/* <Route path="*" element={<PageNotFound />} /> */}
+            <Route path="*" element={<PageNotFound />} /> 
         </Routes>
     </div>
   )
